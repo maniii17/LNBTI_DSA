29d2f80b7584f8ec246bb0728779a456d7890b66
Manindu Ramawickrama <maninduramawickrama110@gmail.com>
2024-05-30 19:24:07 +0530

    railway

---
+++ bubbleSort.java
@@ -0,0 +1,26 @@
+public class BubbleSort{
+    public static void main(String []args){
+        int[] array={12,11,2,18,87,34,27,56,43,33};
+        int i;
+        int j;
+        System.out.println("Before sort");
+        for(i=0; i<array.length-1; i++){
+            System.out.print(array[i]+",");
+        }
+        System.out.println("");
+        for(i=1; i<array.length-1;i++){
+            for(j=0; j<array.length-i;j++){
+                 if(array[j]>array[j+1]){
+                    int temp=array[j];
+                    array[j]=array[j+1];
+                     array[j+1]=temp;
+                 }
+            }
+        }
+        System.out.println("after sort");
+        for(i=0; i<array.length-1; i++){
+            System.out.print(array[i]+",");
+        }
+
+    }
+}
