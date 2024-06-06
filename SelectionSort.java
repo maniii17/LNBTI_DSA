1b7d839fce54cf8ac03d227ae455dd2835f8f726
Manindu Ramawickrama <maninduramawickrama110@gmail.com>
2024-06-06 19:13:16 +0530

    railway
    This repository contains a simple implementation of the Selection Sort algorithm in Java. Selection Sort is a straightforward and intuitive algorithm that sorts an array by repeatedly finding the minimum element from the unsorted part and putting it at the beginning.

---
+++ SelectionSort.java
@@ -0,0 +1,39 @@
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+ 
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
